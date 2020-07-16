9e93593dc02265cd2e44c68917f40ff840ec883d
Andrzej Więcek <andrzej.wiecek94@gmail.com>
2020-07-16 22:39:17 +0200

    Create script.js
    - created functionalities for the Todo List

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+//Check off specific todos by clicking on them
+$("ul").on("click", "li", function (){
+    $(this).toggleClass('completed');
+});
+
+//Fading out specific todos by clicking on the span
+$("ul").on("click", "span", function (event) { 
+    $(this).parent().fadeOut(500, function(){
+       $(this).remove(); 
+    });
+    event.stopPropagation();    
+});
+
+//Add a new todo
+$('input[type="text"]').keypress(function(e){
+    if(e.which ===13){
+        let todoText = $(this).val();
+        $(this).val("");
+        //Create new li and add it to the ul
+        $("ul").append("<li><span> <i class='icon-trash'></i> </span>" + todoText + "</li>");
+    }
+});
+
+//Toggle the input
+$('.icon-plus').click(function(){
+    $('input[type="text"]').slideToggle(700);
+})
